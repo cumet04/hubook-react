9bcbcb1fcaf9d77bc452211851aa92917391127c
cumet04 <medalhkr@gmail.com>
2020-06-17 23:00:26 +0900

    base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import css from "./App.module.css";
 
 function App() {
   return (
-    <div className={css.App}>
-      <header className={css.App_header}>
-        <img src={logo} className={css.App_logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={css.App_link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className={css.header}>
+        <span>hubook</span>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+import "ress";
+import "./index.css";
+
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
