be0884ce4b7da44ff95590ef9defe8e73279143b
medalhkr <medalhkr@gmail.com>
2020-11-03 14:13:44 +0900

    use tailwind 2

--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -4,23 +4,21 @@ import { mdiCog } from "@mdi/js";
 import Icon from "@mdi/react";
 
 export default function TheHeader() {
-  const prefHoverStyle =
-    "rounded-full transition-colors duration-200 bg-white hover:bg-gray-400";
   return (
-    <header
-      className="flex items-center h-12 px-6 border-solid border-gray-400"
-      style={{ borderBottomWidth: "1px" }}
-    >
+    <header className="flex items-center h-12 px-6 border-bottom-1 border-gray-400">
       <Link to="/" className="text-lg">
         hubook
       </Link>
       <div className="flex-grow"></div>
       <Link
         to="/preferences"
-        className={`grid place-items-center w-8 h-8 ${prefHoverStyle}`}
+        className={`grid place-items-center w-8 h-8 ${sPreferenceHover}`}
       >
         <Icon path={mdiCog} color="dimgray" className="w-6 h-6 inset-auto" />
       </Link>
     </header>
   );
 }
+
+const sPreferenceHover =
+  "rounded-full transition-colors duration-200 bg-white hover:bg-gray-400";
